a42c994beb319409f8ae46aca627a97078bdfb54
Andrew Buultjens <buultjensa@gmail.com>
2022-06-06 10:58:46 +1000

    Add files via upload

---
+++ MP-MP_RUNNER.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+for US in $(cat US_list.txt); do
+
+        for CD in $(cat CD_list.txt); do
+
+		PREFIX=MP-MP_US-${US}_CD-${CD}
+
+		Rscript MP-MP.R \
+			${US} \
+			${CD} \
+			data/20210811_Extract4Nick.csv \
+			${PREFIX}_predictions_to_evaluate.csv \
+			${PREFIX}_AUC.csv \
+			${PREFIX}_stats.csv \
+			${PREFIX}_matrix.csv \
+			${PREFIX}_any.csv \
+			${PREFIX}_pred_any.csv \
+			${PREFIX} \
+			${PREFIX}_ranking.csv
+        
+	done
+
+done
